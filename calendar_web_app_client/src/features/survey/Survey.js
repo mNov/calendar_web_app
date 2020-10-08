74fc95518a2200ef7eb3082ea29b26b26de07f37
Michal Novemsky <michal.novemsky@gmail.com>
2020-10-08 15:59:57 -0400

    use redux store, get a basic question flow going

--- calendar_web_app_client/src/features/survey/Survey.js
+++ calendar_web_app_client/src/features/survey/Survey.js
@@ -1,56 +1,105 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectCurrentQuestion,
+  updateAnswer,
+  updateCurrentQuestion,
+} from "./surveySlice";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function Question(props) {
+  const dispatch = useDispatch();
+  const [answer, setAnswer] = useState(null);
+  const currentQuestion = useSelector(selectCurrentQuestion);
+  return currentQuestion == props.questionName ? (
+    <>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(updateAnswer(props.questionName, answer));
+          dispatch(updateCurrentQuestion());
+        }}
+      >
+        <Form.Group>
+          <Form.Label>{props.questionString}</Form.Label>
+          <Form.Check
+            inline
+            type="radio"
+            label={"Yes"}
+            value={true}
+            checked={answer === "true"}
+            onChange={(e) => setAnswer(e.target.value)}
+          />
+          <Form.Check
+            inline
+            type="radio"
+            label={"No"}
+            value={false}
+            checked={answer === "false"}
+            onChange={(e) => setAnswer(e.target.value)}
+          />
+          <Form.Text className="text-muted">{props.questionText}</Form.Text>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Next question
+        </Button>
+      </Form>
+    </>
+  ) : null;
+}
 
 export function Survey() {
-  const [surveyAnswers, setSurveyAnswers] = useState({
-    personalHistory: null,
-    familyHistory: null,
-    geneticMutation: null,
-    radiationBefore30: null,
-    below40: null,
-    between40And44: null,
-    between45And54: null,
-    above55: null,
-  });
-
-  const [currentQuestion, setCurrentQuestion] = useState(1);
-
-  const handleSurveyAnswerChange = (questionName) => (e) => {
-    setSurveyAnswers({
-      ...surveyAnswers,
-      [questionName]: e.target.value,
-    });
-    // console.log("the surveyanswers state is now");
-    // console.dir(surveyAnswers);
-  };
-
+  const currentQuestion = useSelector(selectCurrentQuestion);
   return (
-    <Form>
-      <Form.Group>
-        <Form.Label>
-          First question: do you have a history of breast cancer?
-        </Form.Label>
-        <Form.Check
-          inline
-          type="radio"
-          label={"Yes"}
-          value={true}
-          checked={surveyAnswers.personalHistory === "true"}
-          onChange={handleSurveyAnswerChange("personalHistory")}
-        />
-        <Form.Check
-          inline
-          type="radio"
-          label={"No"}
-          value={false}
-          checked={surveyAnswers.personalHistory === "false"}
-          onChange={handleSurveyAnswerChange("personalHistory")}
-        />
-        <Form.Text className="text-muted">
-          If you have ever had breast cancer, or are currently in treatment,
-          select "yes".
-        </Form.Text>
-      </Form.Group>
-    </Form>
+    <>
+      <Question
+        questionString="First question: do you have a history of breast cancer?"
+        questionDesc="If you have ever had breast cancer, or are currently in treatment, select 'yes'."
+        questionName="personalHistory"
+      />
+      <Question
+        questionString="Next question: do you have a family history of breast cancer?"
+        questionDesc="If anyone in your family has had breast cancer, select 'yes'."
+        questionName="familyHistory"
+      />
+      <Question
+        questionString="Next question: do you have a genetic mutation related to breast cancer?"
+        questionDesc="For example, BRCA1 or BRCA2"
+        questionName="geneticMutation"
+      />
+      <Question
+        questionString="Next question: have you had radiation to your chest before age 30?"
+        questionDesc="This would most likely be from cancer treatment, but could also be from certain types of scans (???)"
+        questionName="radiationBefore30"
+      />
+      <Question
+        questionString="Next question: are you below the age of 40?"
+        questionDesc="Ages 0-39"
+        questionName="below40"
+      />
+      <Question
+        questionString="Next question: are you between the ages of 40-44?"
+        questionDesc="Ages 40-44"
+        questionName="between40And44"
+      />
+      <Question
+        questionString="Next question: are you between the ages of 45-54?"
+        questionDesc="Ages 45-54"
+        questionName="between45And54"
+      />
+      <Question
+        questionString="Next question: are you 55 or older?"
+        questionDesc="Ages 55+"
+        questionName="above55"
+      />
+      {currentQuestion == "end" ? (
+        <div>
+          You've reached the end of the survey! Here are your recommendations,
+          which you can add to your Google calendar here. (Insert link to Google
+          calendar)
+        </div>
+      ) : null}
+    </>
   );
 }
--- calendar_web_app_client/src/features/survey/surveySlice.js
+++ calendar_web_app_client/src/features/survey/surveySlice.js
@@ -3,9 +3,58 @@ import { createSlice } from "@reduxjs/toolkit";
 export const surveySlice = createSlice({
   name: "survey",
   initialState: {
-    history: null,
+    personalHistory: null,
+    familyHistory: null,
+    geneticMutation: null,
+    radiationBefore30: null,
+    // TODO - maybe don't ask yes/no q's for all 4 of these
+    below40: null,
+    between40And44: null,
+    between45And54: null,
+    above55: null,
+    currentQuestion: "personalHistory",
   },
-  reducers: {},
+  reducers: {
+    updateAnswer: (state, action) => {
+      state[action.payload.question] = action.payload.answer;
+    },
+    updateCurrentQuestion: (state) => {
+      // tree logic goes here - TODO - this should be better
+      if (state.currentQuestion == "personalHistory") {
+        state.currentQuestion = "familyHistory";
+      } else if (state.currentQuestion == "familyHistory") {
+        state.currentQuestion = "geneticMutation";
+      } else if (state.currentQuestion == "geneticMutation") {
+        state.currentQuestion = "radiationBefore30";
+      } else if (state.currentQuestion == "radiationBefore30") {
+        state.currentQuestion = "below40";
+      } else if (state.currentQuestion == "below40") {
+        if (state.below40 === false) {
+          state.currentQuestion = "between40And44";
+        } else {
+          state.currentQuestion = "end";
+        }
+      } else if (state.currentQuestion == "between40And44") {
+        if (state.between40And44 == false) {
+          state.currentQuestion = "between45And54";
+        } else {
+          state.currentQuestion = "end";
+        }
+      } else if (state.currentQuestion == "between45And54") {
+        if (state.between45And54 == false) {
+          state.currentQuestion = "above55"; // TODO - is this necessary?
+        } else {
+          state.currentQuestion = "end";
+        }
+      } else {
+        state.currentQuestion = "end";
+      }
+    },
+  },
 });
+
+export const { updateAnswer, updateCurrentQuestion } = surveySlice.actions;
+
+export const selectCurrentQuestion = (state) => state.survey.currentQuestion;
 
 export default surveySlice.reducer;
